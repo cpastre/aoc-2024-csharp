5bc9c10db9623f2de6070fe314e79db1a7028375
Charlie Pastre <charlie@pastre.com>
2024-12-06 14:47:02 -0800

    1. Day04 Part 1 verified

--- Day04/Program - Day04.cs
+++ Day04/Program - Day04.cs
@@ -4,6 +4,7 @@
     {
         record Direction
         {
+            public Compass compass;
             public required Func<int, int, int> iOperation;
             public required Func<int, int, int> jOperation;
         }
@@ -12,6 +13,12 @@
         {
             public required int i;
             public required int j;
+        }
+
+        record Found
+        {
+            public required Location location;
+            public required Direction direction;
         }
 
         enum Compass { E, NE, N, NW, W, SW, S, SE };
@@ -24,33 +31,29 @@
             Func<int, int, int> minusOp = (int i1, int i2) => i1 - i2;
             Func<int, int, int> zeroOp = (int i1, int i2) => i1;
             Dictionary<Compass, Direction> Directions = new Dictionary<Compass, Direction> {
-                [Compass.E] = new Direction { iOperation = zeroOp, jOperation = plusOp },  //E
-                [Compass.NE] = new Direction { iOperation = minusOp, jOperation = plusOp },  //NE
-                [Compass.N] = new Direction { iOperation = minusOp, jOperation = zeroOp },  //N
-                [Compass.NW] = new Direction { iOperation = minusOp, jOperation = minusOp },  //NW
-                [Compass.W] = new Direction { iOperation = zeroOp, jOperation = minusOp },  //W
-                [Compass.SW] = new Direction { iOperation = minusOp, jOperation = plusOp },  //SW
-                [Compass.S] = new Direction { iOperation = minusOp, jOperation = zeroOp },  //S
-                [Compass.SE] = new Direction { iOperation = plusOp, jOperation = plusOp }  //SE
-                };
+                [Compass.E] = new Direction { compass = Compass.E, iOperation = zeroOp, jOperation = plusOp },  //E
+                [Compass.NE] = new Direction { compass = Compass.NE, iOperation = minusOp, jOperation = plusOp },  //NE
+                [Compass.N] = new Direction { compass = Compass.N, iOperation = minusOp, jOperation = zeroOp },  //N
+                [Compass.NW] = new Direction { compass = Compass.NW, iOperation = minusOp, jOperation = minusOp },  //NW
+                [Compass.W] = new Direction { compass = Compass.W, iOperation = zeroOp, jOperation = minusOp },  //W
+                [Compass.SW] = new Direction { compass = Compass.SW, iOperation = plusOp, jOperation = minusOp },  //SW
+                [Compass.S] = new Direction { compass = Compass.S, iOperation = plusOp, jOperation = zeroOp },  //S
+                [Compass.SE] = new Direction { compass = Compass.SE, iOperation = plusOp, jOperation = plusOp }  //SE
+            };
 
             var countXmases = (string[] input) => CountFoundStrings(input, Directions, "XMAS");
 
-            int testCount = countXmases(testInput);
+            //int testCount = countXmases(testInput);
 
             Console.WriteLine("--- PART ONE ---\n");
             Console.WriteLine($"The number of found XMASes is: {countXmases(liveInput)}.");
             Console.WriteLine();
             Console.WriteLine($"Test reports: {countXmases(testInput)}.");
-
-)
-
-            
         }
 
         private static int CountFoundStrings(string[] input, Dictionary<Compass, Direction> directions, string findString)
         {
-            int findCount = 0;
+            List<Found> founds = [];
             //each row
             for (int i = 0; i < input.Length; i++)
             {
@@ -60,31 +63,34 @@
                     //each direction
                     foreach (var direction in directions)
                     {
-                        string crawlDirection (string[] input, Direction direction, Location startLocation, int length) {
-                            string crawlDirectionIter(Location location, int length)
+                        string crawlDirection (string[] input, Direction aimDirection, Location startLocation, int length) {
+                            string crawlDirectionIter(Location iterLocation, int iterLength)
                             {
-                                if (location.i < 0 || location.j < 0
-                                    || location.i > input.Length - 1 ||  location.j > input[i].Length - 1)
+                                if (iterLength == 0 ||
+                                    iterLocation.i < 0 || iterLocation.j < 0
+                                    || iterLocation.i > input.Length - 1 ||  iterLocation.j > input[i].Length - 1)
                                 {
                                     return string.Empty;
                                 }
                                 else
                                 {
-                                    return crawlDirectionIter(MoveLocation(location, direction, 1), 1) + input[i][j];
+                                    return input[iterLocation.i][iterLocation.j] 
+                                        + crawlDirectionIter(MoveLocation(iterLocation, aimDirection, 1), --iterLength);
                                 }
                             }
-                            return crawlDirectionIter(startLocation, 1);
+                            return crawlDirectionIter(startLocation, length);
                         }
 
-                        if (crawlDirection(input, direction, new Location { i = i, j = j }, findString.Length)
-                            == findString)
+                        string found = crawlDirection(input, direction.Value, new Location { i = i, j = j }, findString.Length);
+
+                        if (found == findString)
                         {
-                            findCount++;
+                            founds.Add(new Found { location = new Location { i = i, j = j }, direction = direction.Value });
                         }
                     }
                 }
             }
-            return findCount;
+            return founds.Count;
         }
 
         static Location MoveLocation(Location location, Direction direction, int distance)
