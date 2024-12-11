556a45f27812f9bfd31b8082fabd42ce695eba6f
Charlie Pastre <charlie@pastre.com>
2024-12-10 20:04:15 -0800

    1. Day05 read in

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -2,9 +2,23 @@
 {
     internal class Program
     {
+        public record OrderLine
+        {
+            public required int EarlyPage;
+            public required int LaterPage;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] inputLive = File.ReadAllLines(@"input.txt");
+            string[] inputTest = File.ReadAllLines(@"input - sample.txt");
+
+            var orderLines = inputLive
+                .Where(l => l.Contains("|"))
+                .Select(l => new OrderLine { EarlyPage = int.Parse(l.Split("|")[0]), LaterPage = int.Parse(l.Split("|")[1]) });
+            var updatePages = inputLive
+                .Where(l => l.Contains(","))
+                .Select(l => l.Split(",").Select(p => int.Parse(p)));
         }
     }
 }
