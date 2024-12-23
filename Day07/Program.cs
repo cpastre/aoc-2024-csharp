82baf1f432ad7d4858cbc9874167ed0acdb639ad
Charlie Pastre <charlie@pastre.com>
2024-12-22 16:37:51 -0800

    Starting on Day07

---
+++ Day07/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Collections.ObjectModel;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Day07
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var loadInputFile = (string filename) => new ReadOnlyCollection<string>(File.ReadAllLines(filename));
+            var testInput = loadInputFile("input - Sample.txt");
+            var liveInput = loadInputFile("input.txt");
+
+            Console.WriteLine("--- PART ONE ---\n");
+            Console.WriteLine("Test Values");
+            ProcessPart1(testInput);
+            //Console.WriteLine("Live Values");
+            //ProcessPart1(liveInput);
+        }
+
+        private static void ProcessPart1(ReadOnlyCollection<string> input)
+        {
+            
+        }
+    }
+}
