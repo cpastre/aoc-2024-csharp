894c29d0453744622f6e2244f5e22007e32d1234
Charlie Pastre <charlie@pastre.com>
2024-12-22 17:00:24 -0800

    LoadEquationSource

--- Day07/Program.cs
+++ Day07/Program.cs
@@ -1,11 +1,18 @@
 ﻿
 using System.Collections.ObjectModel;
 using System.Reflection.Metadata.Ecma335;
+using System.Text.RegularExpressions;
 
 namespace Day07
 {
     internal class Program
     {
+        private record EquationSource
+        {
+            public required int Sum;
+            public required int[] Numbers;
+        }
+
         static void Main()
         {
             var loadInputFile = (string filename) => new ReadOnlyCollection<string>(File.ReadAllLines(filename));
@@ -21,7 +28,14 @@ namespace Day07
 
         private static void ProcessPart1(ReadOnlyCollection<string> input)
         {
-            
+            var loadEquationSource = () => input.Select(e =>
+                new EquationSource
+                {
+                    Sum = int.Parse(e.Substring(0, e.IndexOf(':'))),
+                    Numbers = Regex.Matches(e.Substring(e.IndexOf(':') + 1), @"\b(\w+)")
+                                .Select(n => int.Parse(n.Value)).ToArray()
+                });
+            var t1 = loadEquationSource();
         }
     }
 }
